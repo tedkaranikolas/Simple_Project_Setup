e5433ecfb54b9b7c24edf8f525493920152794ce
Ted Karanikolas <ted@hrg-nyc.com>
2016-05-25 01:18:28 -0500

    Initial Commit

---
+++ scripts/myjavascript.js
@@ -0,0 +1,31 @@
+//function whosWho () {if ( "Ezra Pound" ){
+  //console.log("Yup, good ol' Ez.");
+//}
+//}
+
+//} else if ( "Peter Doherty" ){
+  //console.log( "Nope.  Not in this tower." );
+
+//} else if ( "Wyndham Lewis"){
+  //console.log("Pals, not tower enemies.");
+
+//} else { console.log ("Peter's buddy, not Thomas Stern Eliot's.");
+//}
+//}
+//whosWho();
+function whosEzra (){
+  alert("Yup, it IS gold ol' Ez");
+  console.log("Yup, it IS gold ol' Ez");
+}
+function whosPete (){
+  alert("Nope.  Not in this tower!");
+  console.log("Nope.  Not in this tower!");
+}
+function whosWyndham(){
+  alert("Nope, he was Blasting and Bombardiering.");
+  console.log("Nope, he was Blasting and Bombardiering.");
+}
+function whosCarlos(){
+  alert("He's Pete's buddy, not Bobby's.");
+  console.log("He's Pete's buddy, not Bobby's.");
+}
